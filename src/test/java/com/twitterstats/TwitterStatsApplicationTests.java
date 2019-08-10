987d95577fe18223c4e709ba15bd1e8c2cc5ebdb
Dan Sullivan <drsullivan13@gmail.com>
2019-08-10 07:49:40 -0400

    first commit

---
+++ src/test/java/com/twitterstats/TwitterStatsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twitterstats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterStatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
